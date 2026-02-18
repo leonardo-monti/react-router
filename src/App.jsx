7f781b1c000ae6725646ac5147ea34e6585e77e0
leonardo-monti <monti.leonardo22@gmail.com>
2026-02-18 17:31:48 +0100

    add products

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from './pages/HomePage';
 import Navbar from './components/Navbar';
--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,13 +1,39 @@
+import { useEffect, useState } from "react"
+import axios from "axios"
+
 export default function ProductsPage() {
+
+    const [products, setProducts] = useState([])
+
+    useEffect(() => {
+        axios.get("https://fakestoreapi.com/products")
+            .then((res) => {
+                setProducts(res.data)
+            })
+    }, [])
+
     return (
         <div>
+            <h1 className="text-center mb-4">Prodotti</h1>
 
-            <h1 className="text-center">Prodotti</h1>
-            <p className="text-center">...</p>
+            <div className="row g-3">
+                {products.map((p) => (
+                    <div key={p.id} className="col-12 col-sm-6 col-md-4 col-lg-3">
+                        <div className="card h-100">
+                            <img src={p.image}
+                                alt={p.title}
+                                className="card-img-top p-3"
+                                style={{ height: "180px", objectFit: "contain" }}
+                            />
+                            <div className="card-body d-flex flex-column">
+                                <h6 className="text-center">{p.title}</h6>
+                                <p className="text-center text-muted">{p.description}</p>
+                                <p className="text-center fw-bold">{p.price} €</p>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
         </div>
-
-
     )
-
-
 }
