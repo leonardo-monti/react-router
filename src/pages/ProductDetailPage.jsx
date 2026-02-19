8e8cf9d274aeb0c972ddbc94dceb96fd71ec2285
leonardo-monti <monti.leonardo22@gmail.com>
2026-02-19 16:26:49 +0100

    end

--- src/pages/ProductDetailPage.jsx
+++ src/pages/ProductDetailPage.jsx
@@ -1,7 +1,49 @@
+import axios from "axios"
+import { useState, useEffect } from "react"
+import { useParams } from "react-router-dom"
+
+
 export default function ProductDetailPage() {
+
+    const { id } = useParams()
+
+    const [product, setProduct] = useState(null)
+
+    useEffect(() => {
+        axios.get(`https://fakestoreapi.com/products/${id}`)
+            .then((res) => {
+                setProduct(res.data)
+            })
+
+    }, [id])
+
+    if (!product) {
+        return <h1>Loading...</h1>
+    }
+
+
     return (
 
-        <h1>Prodotto..</h1>
+        <div className="row g-4">
+            <div className="col-md-5">
+                <div className="card shadow p-4">
+                    <img
+                        src={product.image}
+                        alt={product.title}
+                        className="img-fluid"
+                        style={{ height: "350px", objectFit: "contain" }} />
+                </div>
+            </div>
+            <div className="col-md-7">
+                <h1>{product.title}</h1>
+                <p>{product.description}</p>
+                <p className="fw-bold">{product.price}€</p>
+                <button className="mt-3 btn btn-dark">Aggiungi al carrello</button>
+            </div>
+        </div>
+
+
+
     )
 
 }
--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react"
 import axios from "axios"
+import { Link } from "react-router-dom"
 
 export default function ProductsPage() {
 
@@ -19,20 +20,23 @@ export default function ProductsPage() {
             <div className="row g-3">
                 {products.map((p) => (
                     <div key={p.id} className="col-12 col-sm-6 col-md-4 col-lg-3">
-                        <div className="card h-100 shadow-sm">
-                            <img src={p.image}
-                                alt={p.title}
-                                className="card-img-top p-3"
-                                style={{ height: "180px", objectFit: "contain" }}
-                            />
-                            <div className="card-body d-flex flex-column">
-                                <h6 className="text-center">{p.title}</h6>
-                                <p className="text-center fw-bold">{p.price} €</p>
+                        <Link to={`/prodotti/${p.id}`}
+                            className="text-decoration-none">
+                            <div className="card h-100 shadow-sm">
+                                <img src={p.image}
+                                    alt={p.title}
+                                    className="card-img-top p-3"
+                                    style={{ height: "180px", objectFit: "contain" }}
+                                />
+                                <div className="card-body d-flex flex-column">
+                                    <h6 className="text-center">{p.title}</h6>
+                                    <p className="text-center fw-bold">{p.price} €</p>
+                                </div>
                             </div>
-                        </div>
+                        </Link>
                     </div>
                 ))}
             </div>
-        </div>
+        </div >
     )
 }
